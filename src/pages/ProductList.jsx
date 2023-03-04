eed4476ee391ae6bffa2f502b062113c7a56a591
Kübra Çavuşoğlu <118998624+kubracvs@users.noreply.github.com>
2023-03-04 12:18:32 +0300

    first commit

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,49 @@
+
+import React,{useState,useEffect} from "react";
+import ProductCard from "../components/ProductCard";
+import CardTotal from "../components/CardTotal";
+import axios from "axios";
+
+
+const ProductList = () => {
+  const url = process.env.REACT_APP_API_URL;
+
+
+  const [products,setProducts] = useState([])
+ 
+  const getProducts = async () =>{
+    console.log('merhaba');
+    
+    try {
+      const { data } = await axios(url)
+      setProducts(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div className="container mt-3">
+      <div className={"bg-light d-sm-block d-md-flex"}>
+        <p className="text-center text-danger w-100">Loading....</p>
+
+        <>
+          <article id="product-panel" className="col-md-5">
+            <ProductCard />
+          </article>
+          <article className="col-md-5 m-3">
+            <CardTotal />
+          </article>
+        </>
+
+        <p className="text-center text-danger w-100">No products data...</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
